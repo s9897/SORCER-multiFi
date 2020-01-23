d08ca5ca888b95e9f51680378d354026ff8a250b
s19650 <49076068+s19650@users.noreply.github.com>
2020-01-23 23:12:32 +0100

    Add files via upload

---
+++ Homework/Modify.java
@@ -0,0 +1,23 @@
+public interface Modify {
+
+   	public Context AddBrownSugar(Context context) throws RemoteException, ContextException; 
+	
+	
+	public Context AddWhiteSugar(Context context ) throws RemoteException, ContextException;
+	
+	
+	public Context CountSugarCubes(Context context) throws RemoteException, ContextException;
+	
+	
+	public Context AddMilk(Context context) throws RemoteException, ContextException;
+	
+	
+	public Context AddMilkCoconut(Context context) throws RemoteException, ContextException; 
+	
+	
+	public Context AddMilkRice(Context context) throws RemoteException, ContextException;
+	
+	
+	public Context AddExtraIngridient(Context context) throws RemoteException, ContextException;
+		
+}
